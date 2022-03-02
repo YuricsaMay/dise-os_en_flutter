dc42d87a2e711399c5caf7709d1ffc74a6206342
YuricsaMay <75384236+YuricsaMay@users.noreply.github.com>
2022-03-02 18:58:39 -0500

    diseño compuesto bottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,9 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
-     statusBarColor: Colors.black
-    ));
+    SystemChrome.setSystemUIOverlayStyle( SystemUiOverlayStyle.light.copyWith(
+      statusBarColor: Colors.black
+));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
